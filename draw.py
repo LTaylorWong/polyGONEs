3214c60a4079d01e8b2b2a8dc519db3be7cf79fc
lian wong <ltaylor.wong@gmail.com>
2016-04-11 11:19:26 -0400

    stuff

--- draw.py
+++ draw.py
@@ -42,8 +42,8 @@ def add_box( points, x, y, z, width, height, depth ):
                  x, y1, z1, 
                  x, y1, z )
     add_polygon( points,
-                 x, y, z
-                 x1, y, z1, 
+                 x, y, z,
+                 x1 , y, z1, 
                  x, y, z1 )
     add_polygon( points, 
                  x, y1, z1, 
@@ -54,7 +54,7 @@ def add_box( points, x, y, z, width, height, depth ):
                  x1, y1, z1, 
                  x, y1, z1 )
     add_polygon( points,
-                 x1,y1,z
+                 x1,y1,z,
                  x1, y1, z1, 
                  x, y1, z1 )
 
