e149f96fbcfc2c60da228a3016b6fd5ed9639a3a
lian wong <ltaylor.wong@gmail.com>
2016-04-11 11:17:04 -0400

    stuff

--- draw.py
+++ draw.py
@@ -25,30 +25,38 @@ def add_box( points, x, y, z, width, height, depth ):
     y1 = y - height
     z1 = z - depth
 
-    add_edge( points, 
-              x, y, z, 
-              x, y, z )
-    add_edge( points, 
-              x, y1, z, 
-              x, y1, z )
-    add_edge( points, 
-              x1, y, z, 
-              x1, y, z )
-    add_edge( points, 
-              x1, y1, z, 
-              x1, y1, z )
-    add_edge( points, 
-              x, y, z1, 
-              x, y, z1 )
-    add_edge( points, 
-              x, y1, z1, 
-              x, y1, z1 )
-    add_edge( points, 
-              x1, y, z1, 
-              x1, y, z1 )
-    add_edge( points, 
-              x1, y1, z1, 
-              x1, y1, z1 )
+    add_polygon( points, 
+                 x, y, z, 
+                 x, y1, z,
+                 x1,y,z)
+    add_polygon( points, 
+                 x, y1, z, 
+                 x1, y1, z,
+                 x1,y,z)
+    add_polygon( points,
+                 x,y,z,
+                 x, y, z1, 
+                 x, y1, z )
+    add_polygon( points,
+                 x,y,z1,
+                 x, y1, z1, 
+                 x, y1, z )
+    add_polygon( points,
+                 x, y, z
+                 x1, y, z1, 
+                 x, y, z1 )
+    add_polygon( points, 
+                 x, y1, z1, 
+                 x1, y, z1,
+                 x, y1, z1)
+    add_polygon( points,
+                 x,y1,z,
+                 x1, y1, z1, 
+                 x, y1, z1 )
+    add_polygon( points,
+                 x1,y1,z
+                 x1, y1, z1, 
+                 x, y1, z1 )
 
 def add_sphere( points, cx, cy, cz, r, step ):
     
